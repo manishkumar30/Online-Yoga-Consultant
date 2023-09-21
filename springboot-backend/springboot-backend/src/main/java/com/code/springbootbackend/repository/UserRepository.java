3e46aa5c5864c5cd7d0a6df7970cbef3a450922c
Manish Kumar <87971964+manishkumar30@users.noreply.github.com>
2023-09-21 20:58:47 +0530

    Delete springboot-backend/springboot-backend directory

--- springboot-backend/springboot-backend/src/main/java/com/code/springbootbackend/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.code.springbootbackend.repository;
-
-import com.code.springbootbackend.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
